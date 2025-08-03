e287b84deabf4b6008245663417acd87577e338c
Cody Blevins <cody@example.com>
2025-08-03 18:28:42 -0400

    chore(husky): relax commit subject length limit

--- cypress.config.ts
+++ cypress.config.ts
@@ -21,6 +21,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:1420",
+    supportFile: false, // Disable support file requirement
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
